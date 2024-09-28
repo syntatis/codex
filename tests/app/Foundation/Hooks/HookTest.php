a66e348b182fd946e7885252531c5a94269fbb80
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-09-28 23:34:08 +0700

    Refactor hook custom ID (#12)

--- tests/app/Foundation/Hooks/HookTest.php
+++ tests/app/Foundation/Hooks/HookTest.php
@@ -182,7 +182,7 @@ class HookTest extends WPTestCase
 
 		$this->expectException(InvalidArgumentException::class);
 
-		$this->hook->addAction('wp_footer', $func, 70, 1, ['id' => '@bar']);
+		$this->hook->addAction('wp_footer', $func, 70, 1, ['id' => '#bar']);
 	}
 
 	/** @group with-ref */
@@ -193,7 +193,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(50, has_action('register_sidebar', $func));
 
-		$this->hook->removeAction('register_sidebar', '@bar', 50);
+		$this->hook->removeAction('register_sidebar', '#bar', 50);
 
 		$this->assertFalse(has_action('register_sidebar', $func));
 	}
@@ -207,7 +207,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(51, has_action('register_sidebar', $func));
 
-		$this->hook->removeAction('register_sidebar', '@no-bar', 50);
+		$this->hook->removeAction('register_sidebar', '#no-bar', 50);
 
 		$this->assertSame(51, has_action('register_sidebar', $func));
 	}
@@ -229,7 +229,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(40, has_action('get_sidebar', '__return_null'));
 
-		$this->hook->removeAction('get_sidebar', '@named-func-2', 40);
+		$this->hook->removeAction('get_sidebar', '#named-func-2', 40);
 
 		$this->assertFalse(has_action('get_sidebar', '__return_null'));
 	}
@@ -241,7 +241,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(41, has_action('get_sidebar', '__return_true'));
 
-		$this->hook->removeAction('get_sidebar', '@no-name-func-3', 41);
+		$this->hook->removeAction('get_sidebar', '#no-name-func-3', 41);
 
 		$this->assertSame(41, has_action('get_sidebar', '__return_true'));
 	}
@@ -266,7 +266,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(34, has_action('wp_head', [$callback, 'init']));
 
-		$this->hook->removeAction('wp_head', '@class-2', 34);
+		$this->hook->removeAction('wp_head', '#class-2', 34);
 
 		$this->assertFalse(has_action('wp_head', [$callback, 'init']));
 	}
@@ -280,7 +280,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(35, has_action('wp_head', [$callback, 'init']));
 
-		$this->hook->removeAction('wp_head', '@no-class-3', 35);
+		$this->hook->removeAction('wp_head', '#no-class-3', 35);
 
 		$this->assertSame(35, has_action('wp_head', [$callback, 'init']));
 	}
@@ -294,7 +294,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(10, has_filter('icon_dir', $func));
 
-		$this->hook->removeFilter('icon_dir', '@body', 10);
+		$this->hook->removeFilter('icon_dir', '#body', 10);
 
 		$this->assertFalse(has_filter('icon_dir', $func));
 	}
@@ -315,7 +315,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(200, has_action('get_the_excerpt', '__return_null'));
 
-		$this->hook->removeFilter('get_the_excerpt', '@filter-named-func-2', 200);
+		$this->hook->removeFilter('get_the_excerpt', '#filter-named-func-2', 200);
 
 		$this->assertFalse(has_action('get_the_excerpt', '__return_null'));
 	}
@@ -327,7 +327,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(280, has_action('get_the_archive_title', '__return_true'));
 
-		$this->hook->removeFilter('get_the_archive_title', '@no-filter-named-func-3', 280);
+		$this->hook->removeFilter('get_the_archive_title', '#no-filter-named-func-3', 280);
 
 		$this->assertSame(280, has_action('get_the_archive_title', '__return_true'));
 	}
@@ -352,7 +352,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(52, has_filter('the_title', [$callback, 'init']));
 
-		$this->hook->removeFilter('the_title', '@filter-class-2', 52);
+		$this->hook->removeFilter('the_title', '#filter-class-2', 52);
 
 		$this->assertFalse(has_filter('the_title', [$callback, 'init']));
 	}
@@ -366,7 +366,7 @@ class HookTest extends WPTestCase
 
 		$this->assertSame(62, has_filter('body_class', [$callback, 'init']));
 
-		$this->hook->removeFilter('body_class', '@no-filter-class-3', 62);
+		$this->hook->removeFilter('body_class', '#no-filter-class-3', 62);
 
 		$this->assertSame(62, has_filter('body_class', [$callback, 'init']));
 	}
@@ -447,8 +447,8 @@ class HookTest extends WPTestCase
 
 		$this->hook->addAction('wp', $func, 12, 1, ['id' => 'wp.func']);
 
-		$this->assertSame(12, $this->hook->hasAction('wp', '@wp.func'));
-		$this->assertFalse($this->hook->hasAction('wp', '@wp.function'));
+		$this->assertSame(12, $this->hook->hasAction('wp', '#wp.func'));
+		$this->assertFalse($this->hook->hasAction('wp', '#wp.function'));
 	}
 
 	public function testHasFilter(): void
@@ -472,8 +472,8 @@ class HookTest extends WPTestCase
 
 		$this->hook->addFilter('the_content', $func, 12, 1, ['id' => 'the-content.func']);
 
-		$this->assertSame(12, $this->hook->hasFilter('the_content', '@the-content.func'));
-		$this->assertFalse($this->hook->hasFilter('the_content', '@the-content.function'));
+		$this->assertSame(12, $this->hook->hasFilter('the_content', '#the-content.func'));
+		$this->assertFalse($this->hook->hasFilter('the_content', '#the-content.function'));
 	}
 }
 
