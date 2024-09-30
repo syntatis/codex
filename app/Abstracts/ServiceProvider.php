a1065c1e17da7a9788adbd15439d740e4f6cc260
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-09-30 18:19:25 +0700

    Remove register, and Hook object as arg

--- app/Abstracts/ServiceProvider.php
+++ app/Abstracts/ServiceProvider.php
@@ -4,23 +4,14 @@ declare(strict_types=1);
 
 namespace Codex\Abstracts;
 
-use Codex\Foundation\Hooks\Hook;
 use Pimple\Container;
 
 abstract class ServiceProvider
 {
 	protected Container $container;
 
-	protected Hook $hook;
-
-	final public function __construct(Container $container, Hook $hook)
+	final public function __construct(Container $container)
 	{
 		$this->container = $container;
-		$this->hook = $hook;
 	}
-
-	/**
-	 * Register the provider.
-	 */
-	abstract public function register(): void;
 }
